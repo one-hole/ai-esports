1f4b9dfb4640fe6a515228fbbcfaf001169ba8d9
w-zengtao <so.zengtao@email.com>
2018-10-31 17:14:32 +0800

    [ADD] 增加技能

--- app/models/dota2_constant/ability.rb
+++ app/models/dota2_constant/ability.rb
@@ -2,8 +2,17 @@ module Dota2Constant
   class Ability < ApplicationRecord
     self.table_name = "dota2_abilities"
 
-    def self.load_yml
+    before_create do
+      self.lg_avatar = "http://rw-assets.oss-cn-hangzhou.aliyuncs.com/dota2/abilities/#{self.en_name}_lg.png"
+      self.md_avatar = "http://rw-assets.oss-cn-hangzhou.aliyuncs.com/dota2/abilities/#{self.en_name}_md.png"
+      self.sm_avatar = "http://rw-assets.oss-cn-hangzhou.aliyuncs.com/dota2/abilities/#{self.en_name}_sm.png"
+    end
 
+    def self.load_yml
+      abilities = YAML.load_file(Rails.root.to_s + "/lib/constants/abilities.yml")
+      abilities.each_pair do |key, value|
+        self.create(steam_id: key.to_i, en_name: value.to_s)
+      end
     end
   end
 end
