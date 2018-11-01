a88a49479f4ef3cd58228b7af27f2d4599df038e
w-zengtao <so.zengtao@email.com>
2018-11-01 22:19:00 +0800

    [TEMP] TeamVs

--- app/controllers/api/teamvs_controller.rb
+++ app/controllers/api/teamvs_controller.rb
@@ -1,30 +1,44 @@
 module Api
   class TeamvsController < BaseController
 
-    before_action :check
-
+    # before_action :check
     def index
+      load_series
+      url = ""
+      if @series.type == "Dota2Series"
+        url = "http://api.ouresports.com/api/v2/ai/dota2?left_team_id=#{@series.left_team_id}&right_team_id=#{@series.right_team_id}"
+      elsif @series.type == "CsgoSeries"
+        url = ""
+      end
+
+
 
     end
 
     private
-      def load_first_team
+      def load_series
+        @series = MatchSeries.find_by(id: params[:id])
+        raise SeriesNotFoundError unless @series
       end
 
-      def load_second_team
-      end
-
-      def check
-        check_team_params
-        check_team_unsame
-      end
-
-      def check_team_params
-        raise TeamvsParamsError unless (params.fetch(:left_team_id, false) && params.fetch(:right_team_id, false))
-      end
-
-      def check_team_unsame
-        raise SameTeamError if (params.fetch(:left_team_id, false) == params.fetch(:right_team_id, false))
-      end
+    # private
+    #   def load_first_team
+    #   end
+    #
+    #   def load_second_team
+    #   end
+    #
+    #   def check
+    #     check_team_params
+    #     check_team_unsame
+    #   end
+    #
+    #   def check_team_params
+    #     raise TeamvsParamsError unless (params.fetch(:left_team_id, false) && params.fetch(:right_team_id, false))
+    #   end
+    #
+    #   def check_team_unsame
+    #     raise SameTeamError if (params.fetch(:left_team_id, false) == params.fetch(:right_team_id, false))
+    #   end
   end
 end
