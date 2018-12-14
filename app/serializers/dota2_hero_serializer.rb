abd303c09c2ec21900c8f8550ce034b78d6a38e5
w-zengtao <so.zengtao@email.com>
2018-12-14 20:40:56 +0800

    [FIX]

--- app/serializers/dota2_hero_serializer.rb
+++ app/serializers/dota2_hero_serializer.rb
@@ -1,3 +1,7 @@
 class Dota2HeroSerializer < ActiveModel::Serializer
   attributes Dota2Constant::Hero.attribute_names.delete_if { |att| att == "id" }
+
+  def vert_avatar
+     object.vert_avatar.gsub(".png", ".jpg")
+  end
 end
