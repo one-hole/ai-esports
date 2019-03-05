d1c8b8cd0150d28da2dd608c4ecfcfb9ef924b5b
w-zengtao <so.zengtao@email.com>
2019-03-05 13:18:55 +0800

    [FIX] global namespace

--- app/workers/check_live_worker.rb
+++ app/workers/check_live_worker.rb
@@ -6,7 +6,7 @@ class CheckLiveWorker
 
     @redis = Redis.new(db: 12)
     load_ids.each do |pair|
-      resp = DotaLiveService(pair[0], pair[1], pair[2], pair[3]).run
+      resp = ::DotaLiveService(pair[0], pair[1], pair[2], pair[3]).run
       @redis.publish("tenant-websocket", resp)
     end
 
