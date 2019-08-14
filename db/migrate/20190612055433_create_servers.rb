fc1d8aaa80eefc588b37f85669ca2dd43ab943d8
w-zengtao <so.zengtao@gmail.com>
2019-08-14 23:10:44 +0800

    [FIX] hotfix

--- db/migrate/20190612055433_create_servers.rb
+++ db/migrate/20190612055433_create_servers.rb
@@ -1,10 +1,10 @@
 class CreateServers < ActiveRecord::Migration[5.2]
   def change
-    create_table :servers do |t|
-      t.belongs_to :tenant
-      t.string     :host
-      t.string     :tag
-    end
+    # create_table :servers do |t|
+    #   t.belongs_to :tenant
+    #   t.string     :host
+    #   t.string     :tag
+    # end
 
     # add_index(:servers, [:host, :tenant_id], name: "idx_host_server")
   end
