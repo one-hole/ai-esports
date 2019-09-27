ab0f2d29909125fe77caf36630910c215f88104c
w-zengtao <so.zengtao@gmail.com>
2019-09-27 08:07:11 +0800

    [ADD] Delete Useless

--- app/services/clean_redis_service.rb
+++ app/services/clean_redis_service.rb
@@ -6,7 +6,7 @@ class CleanRedisService
     # 最后更新时间是 15 分钟之前 那么就删除
     Live::Battle.all.each do |battle|
       if (now - battle.updated_at.to_time) >= 900
-        battle.destory
+        battle.destroy
       end
     end
 
