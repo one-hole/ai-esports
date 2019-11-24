78b9bceab51da77dc99541b4ebe14b712d8cebab
w-zengtao <so.zengtao@gmail.com>
2019-11-24 23:47:09 +0800

    [ADD] 时区问题 + 抓取 T2 赛程

---
+++ app/workers/fetch/t2_battle_worker.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+module Fetch
+  class T2BattleWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: 'fetch-battle', retry: false
+
+    def perform
+      begin
+        Schedule::T2score.new
+      ensure
+        Fetch::T2BattleWorker.perform_in(1.minute)
+      end
+    end
+  end
+end
