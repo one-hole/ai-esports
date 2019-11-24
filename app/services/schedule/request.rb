40550eec87ed5c189e319d1c72cafabbac9f5e72
w-zengtao <so.zengtao@gmail.com>
2019-11-25 00:16:02 +0800

    [UPDATE] Remove Headers

--- app/services/schedule/request.rb
+++ app/services/schedule/request.rb
@@ -5,18 +5,10 @@ module Schedule
       request = Typhoeus::Request.new(
         url,
         method:  :get,
-        headers: headers(opts),
         params:  params(opts)
       )
 
       resp = request.run
-    end
-
-    def self.headers(opts = {})
-      {
-        "Referer": "https://www.t2score.com/",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0"
-      }
     end
 
     def self.params(opts = {})
