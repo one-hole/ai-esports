5056a5cfe15340e3f0b08568721de3b967df851b
w-zengtao <so.zengtao@gmail.com>
2019-12-09 14:54:50 +0800

    [ADD] Fix Rebegin V2

--- app/models/battles/dota2_battle.rb
+++ app/models/battles/dota2_battle.rb
@@ -7,7 +7,7 @@ class Dota2Battle < Hole::Battle
   def check_match_detail
     # 这里是比分有改变
     if self.changed.include?("left_score") or self.changed.include?("right_score")
-      match = matches.find_by(game_no: (self.left_score + self.right_score))
+      match = matches.where(game_no: (self.left_score + self.right_score)).last
 
       if match
         match.async_fetch_detail
