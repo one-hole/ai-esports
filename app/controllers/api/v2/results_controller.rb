4e6d9d375242a3182d8e021df4153712796b2012
w-zengtao <so.zengtao@gmail.com>
2020-01-06 14:41:23 +0800

    [ADD] CSGO 不允许查看没打的比赛

--- app/controllers/api/v2/results_controller.rb
+++ app/controllers/api/v2/results_controller.rb
@@ -13,30 +13,9 @@ module Api
       def load_match
         @match = Dota2Match.eager_load(:detail, [battle: [:left_team, :right_team]]).where(battle_id: params[:battle_id], game_no: params[:game_no]).last
         raise CurrentMatchNotStart unless @match
-        raise CurrentMatchNotStart if not_start
       end
 
-      def not_start
-        @battle = @match.battle
 
-        if @battle.format == 3
-          if @battle.left_score == 2 && @battle.right_score == 0
-            return true
-          end
-
-          if @battle.left_score == 0 && @battle.right_score == 2
-            return true
-          end
-        end
-
-        if @battle.format == 5
-          if (@battle.left_score - @battle.right_score).abs == 3
-            return true
-          end
-        end
-
-        return false
-      end
     end
   end
 end
